1abe9337bb175a356aaca3fc2166072e04cabaee
Wilhelm <wilhelm.lynch@gmail.com>
2023-08-08 19:34:29 +0200

    Build search input.

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -11,7 +11,7 @@ const ColorModeSwitch = () => {
         colorScheme="green"
         onChange={toggleColorMode}
       />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
