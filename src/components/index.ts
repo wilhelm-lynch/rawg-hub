43af400fa0ebd6ac1bb3365b17e3b33f03f558b4
Wilhelm Lynch <wilhelm.lynch@gmail.com>
2023-08-23 22:25:47 +0200

    Setup routing.

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as GameHeading } from "./GameHeading";
+export { default as PlatformSelector } from "./PlatformSelector";
+export { default as GenreList } from "./GenreList";
+export { default as SortSelector } from "./SortSelector";
+export { default as GameGrid } from "./GameGrid";
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,35 @@
+import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import {
+  GameGrid,
+  GameHeading,
+  GenreList,
+  PlatformSelector,
+  SortSelector,
+} from "../components";
+
+const HomePage = () => {
+  return (
+    <Grid
+      templateAreas={{ base: `"main"`, lg: `"aside main"` }}
+      templateColumns={{ base: "1fr", lg: "250px 1fr" }}
+    >
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <Box paddingLeft={5}>
+          <GameHeading />
+          <HStack spacing={4} marginBottom={5}>
+            <PlatformSelector />
+            <SortSelector />
+          </HStack>
+        </Box>
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default HomePage;
